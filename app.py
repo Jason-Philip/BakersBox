f7fc6e178a75448b14c07c624d87ac3550ebddb8
Jason-Philip <jasonphilipmoore@outlook.com>
2022-01-21 16:13:20 +0000

    Test Deploying to Heroku

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+#app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+#app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+#app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app)
+#mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
