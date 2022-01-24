12f80656336e7c75c96971637b232a32ee9477d6
Jason-Philip <jasonphilipmoore@outlook.com>
2022-01-24 01:16:51 +0000

    Adding a create recipe form, non functional

--- app.py
+++ app.py
@@ -112,6 +112,10 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/create_recipe")
+def create_recipe():
+    return render_template("create_recipe.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
