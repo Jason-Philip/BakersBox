d8de7f1c2e94b2f06a1f8e8b96cbefd4710f4185
Jason-Philip <jasonphilipmoore@outlook.com>
2022-01-25 13:24:43 +0000

    Fix bug where if not signed in, recipes weren't viewable.

--- app.py
+++ app.py
@@ -180,6 +180,8 @@ def recipe_view(recipe_id):
         recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
         user = mongo.db.users.find_one({"name": session["user"]})
     
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    
     return render_template("recipe.html", recipe=recipe)
 
 
